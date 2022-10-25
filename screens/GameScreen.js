0cbba059b657f9ff524b0dabf53a74d56d779fbf
Oscar Olsson <69190482+oscarolsson94@users.noreply.github.com>
2022-10-25 15:19:53 +0200

    adding guessHandler

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,18 +1,49 @@
 import { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import { NumberContainer } from "../components/game/NumberContainer";
+import { PrimaryButton } from "../components/ui/PrimaryButton";
 import { Title } from "../components/ui/Title";
 import { generateRandomBetween } from "../utils/helpers";
 
+let minBoundary = 1;
+let maxBoundary = 100;
+
 export const GameScreen = ({ userNumber }) => {
-  const initialGuess = generateRandomBetween(1, 100, userNumber);
+  const initialGuess = generateRandomBetween(
+    minBoundary,
+    maxBoundary,
+    userNumber
+  );
   const [currentGuess, setCurrentGuess] = useState(initialGuess);
+
+  const nextGuessHandler = (direction) => {
+    if (direction === "lower") {
+      maxBoundary = currentGuess;
+    } else {
+      minBoundary = currentGuess + 1;
+    }
+    const newRndNumber = generateRandomBetween(
+      minBoundary,
+      maxBoundary,
+      currentGuess
+    );
+    setCurrentGuess(newRndNumber);
+  };
+
   return (
     <View style={styles.screen}>
       <Title>Opponent's guess</Title>
       <NumberContainer>{currentGuess}</NumberContainer>
       <View>
         <Text>Higher or lower?</Text>
+        <View>
+          <PrimaryButton onPress={() => nextGuessHandler("lower")}>
+            -
+          </PrimaryButton>
+          <PrimaryButton onPress={() => nextGuessHandler("higher")}>
+            +
+          </PrimaryButton>
+        </View>
       </View>
       <View>{/* LOG ROUNDS */}</View>
     </View>
