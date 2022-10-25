caa379275b0950e386e49833fe0ed1874495c255
Oscar Olsson <69190482+oscarolsson94@users.noreply.github.com>
2022-10-25 11:24:39 +0200

    working with shadows

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -13,8 +13,19 @@ export const StartGameScreen = () => {
 
 const styles = StyleSheet.create({
   inputContainer: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    marginTop: 100,
+    marginHorizontal: 24,
+    padding: 16,
+    backgroundColor: "#72063c",
+    borderRadius: 8,
+    /* android shadow: */
+    elevation: 4,
+    /*  */
+    /* iOS shadow */
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+    /*  */
   },
 });
