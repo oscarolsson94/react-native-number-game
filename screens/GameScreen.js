60a9a348a65288ec3cb9300d503029ecccf1fd61
Oscar Olsson <69190482+oscarolsson94@users.noreply.github.com>
2022-10-25 15:08:18 +0200

    created numberContainer component

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,14 +1,16 @@
 import { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
+import { NumberContainer } from "../components/game/NumberContainer";
 import { Title } from "../components/ui/Title";
 import { generateRandomBetween } from "../utils/helpers";
 
 export const GameScreen = ({ userNumber }) => {
   const initialGuess = generateRandomBetween(1, 100, userNumber);
-  const [currentGuess, setCurrentGuess] = useState(second);
+  const [currentGuess, setCurrentGuess] = useState(initialGuess);
   return (
     <View style={styles.screen}>
       <Title>Opponent's guess</Title>
+      <NumberContainer>{currentGuess}</NumberContainer>
       <View>
         <Text>Higher or lower?</Text>
       </View>
